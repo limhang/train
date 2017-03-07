283eefb2e745368233c17fca059aa9df1e57332e
sanyueyuxincao <lm_hang@163.com>
2017-03-07 17:43:15 +0800

    整理文件

--- data_analysis/data_sql/lagou_sql.py
+++ data_analysis/data_sql/lagou_sql.py
@@ -1,16 +1,16 @@
 import requests
 import json
 from lxml import etree
+import MySQLdb
+import time
 
 url_1 = 'https://www.lagou.com/jobs/positionAjax.json?px=default&city='
 url_2 = '&needAddtionalResult=false&kd='
 headers = {'Host':'www.lagou.com','User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36','Cookie':'user_trace_token=20170208130109-9aaa0404-edbb-11e6-8f61-5254005c3644; LGUID=20170208130109-9aaa0719-edbb-11e6-8f61-5254005c3644; JSESSIONID=F3F361CEED0ACD3F17112753569325C6; _putrc=00C6A03A01332BEB; login=true; unick=%E6%9D%8E%E6%98%8E%E8%88%AA; showExpriedIndex=1; showExpriedCompanyHome=1; showExpriedMyPublish=1; hasDeliver=152; PRE_UTM=; PRE_HOST=; PRE_SITE=; PRE_LAND=https%3A%2F%2Fwww.lagou.com%2Fjobs%2Flist_iOS%3Fpx%3Ddefault%26city%3D%25E6%25B7%25B1%25E5%259C%25B3; _gat=1; TG-TRACK-CODE=index_navigation; SEARCH_ID=4d90963f818446059da0cbf0bdf0b2ba; index_location_city=%E5%B9%BF%E5%B7%9E; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1488005231,1488419706,1488419773,1488504600; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1488510382; _ga=GA1.2.372407027.1486530069; LGSID=20170303104240-11e969ec-ffbb-11e6-a9fb-525400f775ce; LGRID=20170303110621-6110009b-ffbe-11e6-918d-5254005c3644'}
 
 jobs = ['iOS','Android','php','java','python','C++','web']
-citys = ['北京','上海','深圳','杭州','广州']
-
-
-result = {}
+citys = ['北京','上海','深圳','杭州','广州','成都','武汉']
+citys_utf8 = {'北京':'beijing','上海':'shanghai','深圳':'shenzhen','杭州':'hangzhou','广州':'guangzhou','成都':'chengdu','武汉':'wuhan'}
 
 def getCityInfo(city):
 	result_job = {}
@@ -20,15 +20,30 @@ def getCityInfo(city):
 		binary = r.content
 		data = json.loads(binary)
 		count = str(data['content']['positionResult']['totalCount'])
-		result_job[job] = count
-	result[city] = result_job
-	return result
+		createTime = time.strftime('%Y-%m-%d',time.localtime(time.time()))
+		city_utf8 = 'utf8'
+		city_utf8 = citys_utf8[city]
+		mysqlBlock(city_utf8,job,count,createTime)
+
+
+def mysqlBlock(location,job,num,insertTime):
+	sqli="insert into lagou values(%s,%s,%s,%s)"
+	cursor.execute(sqli,(location,job,num,insertTime))
+
+host="localhost"
+user='root'
+passwd='9981aa'
+db='justTest'
+conn=MySQLdb.connect(host,user,passwd,db)
+cursor=conn.cursor()
+
+# cursor.execute("create table lagou(location varchar(40) ,job varchar(20), num varchar(30), createTiem varchar(40))")
 
 for city in citys:
 	cityResult = getCityInfo(city)
 
+cursor.close()
+conn.commit()
+conn.close()
 
-print(cityResult)
-
-
-
+	
--- data_analysis/data_sql/zhaopingou_sql.py
+++ data_analysis/data_sql/zhaopingou_sql.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import time
+import MySQLdb
+
 #最坑的地方在于，城市这个变动的数据值保存在cooike中,就是cookie的zhaopingou_select_city
 
 headers = {'Host':'www.zhaopingou.com','User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36','Cookie':'JSESSIONID=885F4342C726E39A1B4AECDEBE442B24; JSESSIONID=2EB867544CC327AE626EC41B92AF3760; Hm_lvt_b025367b7ecea68f5a43655f7540e177=1488520262,1488521555,1488521558,1488522258; Hm_lpvt_b025367b7ecea68f5a43655f7540e177=1488522988; zhaopingou_select_city='}
@@ -13,36 +15,48 @@ cookie = 'JSESSIONID=885F4342C726E39A1B4AECDEBE442B24; JSESSIONID=2EB867544CC327
 
 payload = {'pageSize':'0','pageNo':'25','keyStrPostion':'iOS','startDegrees':'-1','endDegress':'-1','startAge':'0','endAge':'0','gender':'-1','timeType':'1','startWorkYear':'-1','endWorkYear':'-1','isMember':'-1','cityId':1,'userToken':'8F37BC7626DC3CE0','clientType':2}
 
-
-outfile =open('zhanpingou.json','w')
-
 # data parse
 # cityID{'北京':'1','上海':'2','深圳':'3','杭州':'4','广州':'5'}
 # timetype('三天':'1','一周':'2')
 
 citys = [1,2,3,4,5]
-czcity = ['北京','上海','深圳','杭州','广州']
+czcity = ['beijing','shanghai','shenzhen','hangzhou','guangzhou']
 jobs = ['iOS','Android','php','java','python','C++','web']
 
-result = {}
 
-def getCityInfo(city):
-	result_job = {}
+def mysqlBlock(location,job,num,insertTime):
+	sqli="insert into zhaopingou values(%s,%s,%s,%s)"
+	cursor.execute(sqli,(location,job,num,insertTime))
+
+host="localhost"
+user='root'
+passwd='9981aa'
+db='justTest'
+conn=MySQLdb.connect(host,user,passwd,db)
+cursor=conn.cursor()
+
+cursor.execute("create table zhaopingou(location varchar(40) ,job varchar(20), num varchar(30), createTiem varchar(40))")
+
+
+for city in citys:
 	for job in jobs:
+		# print(city)
+		# print(job)
+		# print(payload)
 		headers['Cookie'] = cookie + str(city)
 		payload['keyStrPostion'] = job
 		r = requests.post(url,headers=headers,data=payload)
 		binary = r.content
 		data = json.loads(binary)
 		count = str(data['total'])
-		result_job[job] = count
-	result[czcity[int(city)-1]] = result_job
-	return result
+		createTime = time.strftime('%Y-%m-%d',time.localtime(time.time()))
+		# print(czcity[int(city)-1] + '近三天' + job + '简历数' + count)
+		mysqlBlock(czcity[int(city)-1],job,count,createTime)
+
+		
+cursor.close()
+conn.commit()
+conn.close()
 
 
-for city in citys:
-	cityResult = getCityInfo(city)
-
-outfile.write(str(cityResult))
-outfile.close()
 
